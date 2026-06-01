2dc8f5d0e517ef144bf3b83143cc88ca5e80d88c
Pedro1santiago <pedro.santigosiqueira@gmail.com>
2026-05-31 22:36:19 -0300

    refactor(migrations): consolidate all DDL into single V1__create_schema.sql

--- SyncDesk/src/main/resources/db/migration/V1__create_departments.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE departments (
-    id   UUID         NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_departments PRIMARY KEY (id),
-    CONSTRAINT uq_departments_name UNIQUE (name)
-);
